5269fd8bba1d2960a66b7090e8cb96b17aee4fb4
NathaliaMdev <nathalia.meira@hotmail.com>
2022-11-28 15:01:45 -0300

    Adding gif

---
+++ style/main.js
@@ -0,0 +1,14 @@
+
+const array = [3,5,5,7,9,9,1,9,2,5,7,9,2,42,12,56,87,34,23,34]
+
+
+const count = (max) => {
+
+    or (var i = 0; i < max; i++) {
+        console.log(i)
+    
+    };
+
+     console.log(i)
+ };
+        
